07f377ca3ca48cff27c8f41aebdaec2ed4a3fe52
Caio Tracera <ctrcra@gmail.com>
2021-10-28 18:04:10 -0300

    feat: add new transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { useState, useCallback } from 'react';
+import Modal from 'react-modal';
+
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  const handleOpenNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(true);
+  }, []);
+
+  const handleCloseNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(false);
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
     </>
   );
 }
