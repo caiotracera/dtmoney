ef987f45bc8588e27fb829f364b2acc5c45d5205
Caio Tracera <ctrcra@gmail.com>
2021-10-27 23:41:09 -0300

    feat: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Header } from './components/Header';
+
 export function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
